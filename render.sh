40357cd864033dedbc5d73ad65534ced047f5ee3
Christian Schmitt <christian.schmitt@iunera.com>
2024-11-06 16:27:29 +0100

    [ci skip] cleanup

--- render.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-for source in ./assets/pieces/raw/*.svg;do
-  target=./assets/pieces/previews/$(basename $source .svg).png
-  svgexport $source $target png 100%
-#  if [[ ! -f $target ]];then
-#    echo $source $target png 100%
-#  fi
-done #| xargs -P 64 -n 4 svgexport
